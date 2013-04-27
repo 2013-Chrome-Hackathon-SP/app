0e4c86361c77852bf2bd76bdc3a4aadb323ca227
Eduardo Rabelo <oieduardorabelo@gmail.com>
2013-04-27 11:52:45 -0300

    ba dun tz

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
  */
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-    width: 300,
-    height: 300
+    width: 600,
+    height: 500
   });
 });
-      
